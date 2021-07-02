b4ab698ef1754d5a57ecb743e20357ec6e4ee34b
Adam Sansone <alsansone25@gmail.com>
2021-07-01 20:52:20 -0500

    layout stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <h1>Star Wars Characters</h1> 
       </header>
+      <div className="cards">
+        <div className="card">
+          <img src="images/luke-skywalker.jpg" alt="luke skywalker" />
+          <h2 className="characterName">Luke Skywalker</h2>
+          <ul className="details">
+            <li>Homeworld</li>
+            <li>Films</li>
+            <li>Species</li>
+          </ul>
+        </div>
+        <div className="card">
+          <img src="images/luke-skywalker.jpg" alt="luke skywalker" />
+          <h2 className="characterName">Luke Skywalker</h2>
+          <ul className="details">
+            <li>Homeworld</li>
+            <li>Films</li>
+            <li>Species</li>
+          </ul>
+        </div>
+        <div className="card">
+          <img src="images/luke-skywalker.jpg" alt="luke skywalker" />
+          <h2 className="characterName">Luke Skywalker</h2>
+          <ul className="details">
+            <li>Homeworld</li>
+            <li>Films</li>
+            <li>Species</li>
+          </ul>
+        </div>
+        <div className="card">
+          <img src="images/luke-skywalker.jpg" alt="luke skywalker" />
+          <h2 className="characterName">Luke Skywalker</h2>
+          <ul className="details">
+            <li>Homeworld</li>
+            <li>Films</li>
+            <li>Species</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
